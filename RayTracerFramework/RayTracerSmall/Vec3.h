a7e91d4669fe949c97a3c32b00b2fd4ad373ac3d
Adam Harris <adam_harris@hotmail.com>
2021-10-05 11:42:54 +0100

    Some efficiency improvements

---
+++ RayTracerFramework/RayTracerSmall/Vec3.h
@@ -0,0 +1,37 @@
+#pragma once
+
+template<typename T>
+class Vec3
+{
+public:
+	T x, y, z;
+	Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
+	Vec3(T xx) : x(xx), y(xx), z(xx) {}
+	Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
+	inline Vec3& normalize()
+	{
+		T nor2 = length2();
+		if (nor2 > 0) {
+			T invNor = 1 / sqrt(nor2);
+			x *= invNor, y *= invNor, z *= invNor;
+		}
+		return *this;
+	}
+	inline Vec3<T> operator * (const T& f) const { return Vec3<T>(x * f, y * f, z * f); }
+	inline Vec3<T> operator * (const Vec3<T>& v) const { return Vec3<T>(x * v.x, y * v.y, z * v.z); }
+	inline T dot(const Vec3<T>& v) const { return x * v.x + y * v.y + z * v.z; }
+	inline Vec3<T> operator - (const Vec3<T>& v) const { return Vec3<T>(x - v.x, y - v.y, z - v.z); }
+	inline Vec3<T> operator + (const Vec3<T>& v) const { return Vec3<T>(x + v.x, y + v.y, z + v.z); }
+	inline Vec3<T>& operator += (const Vec3<T>& v) { x += v.x, y += v.y, z += v.z; return *this; }
+	inline Vec3<T>& operator *= (const Vec3<T>& v) { x *= v.x, y *= v.y, z *= v.z; return *this; }
+	inline Vec3<T> operator - () const { return Vec3<T>(-x, -y, -z); }
+	inline T length2() const { return x * x + y * y + z * z; }
+	inline T length() const { return sqrt(length2()); }
+	friend std::ostream& operator << (std::ostream& os, const Vec3<T>& v)
+	{
+		os << "[" << v.x << " " << v.y << " " << v.z << "]";
+		return os;
+	}
+};
+
+typedef Vec3<float> Vec3f;
--- RayTracerFramework/RayTracerSmall/main.cpp
+++ RayTracerFramework/RayTracerSmall/main.cpp
@@ -33,6 +33,7 @@
 #include <string.h>
 
 #include "Timer.h"
+#include "Vec3.h"
 #include <thread>
 
 #if defined __linux__ || defined __APPLE__
@@ -43,71 +44,35 @@
 #define INFINITY 1e8
 #endif
 
-template<typename T>
-class Vec3
-{
-public:
-	T x, y, z;
-	Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
-	Vec3(T xx) : x(xx), y(xx), z(xx) {}
-	Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
-	Vec3& normalize()
-	{
-		T nor2 = length2();
-		if (nor2 > 0) {
-			T invNor = 1 / sqrt(nor2);
-			x *= invNor, y *= invNor, z *= invNor;
-		}
-		return *this;
-	}
-	Vec3<T> operator * (const T &f) const { return Vec3<T>(x * f, y * f, z * f); }
-	Vec3<T> operator * (const Vec3<T> &v) const { return Vec3<T>(x * v.x, y * v.y, z * v.z); }
-	T dot(const Vec3<T> &v) const { return x * v.x + y * v.y + z * v.z; }
-	Vec3<T> operator - (const Vec3<T> &v) const { return Vec3<T>(x - v.x, y - v.y, z - v.z); }
-	Vec3<T> operator + (const Vec3<T> &v) const { return Vec3<T>(x + v.x, y + v.y, z + v.z); }
-	Vec3<T>& operator += (const Vec3<T> &v) { x += v.x, y += v.y, z += v.z; return *this; }
-	Vec3<T>& operator *= (const Vec3<T> &v) { x *= v.x, y *= v.y, z *= v.z; return *this; }
-	Vec3<T> operator - () const { return Vec3<T>(-x, -y, -z); }
-	T length2() const { return x * x + y * y + z * z; }
-	T length() const { return sqrt(length2()); }
-	friend std::ostream & operator << (std::ostream &os, const Vec3<T> &v)
-	{
-		os << "[" << v.x << " " << v.y << " " << v.z << "]";
-		return os;
-	}
-};
-
-typedef Vec3<float> Vec3f;
-
 class Sphere
 {
 public:
-	Vec3f center;                           /// position of the sphere
-	float radius, radius2;                  /// sphere radius and radius^2
-	Vec3f surfaceColor, emissionColor;      /// surface color and emission (light)
-	float transparency, reflection;         /// surface transparency and reflectivity
+	Vec3f _center;                           /// position of the sphere
+	float _radius, _radiusSqr;                  /// sphere radius and radius^2
+	Vec3f _surfaceColor, _emissionColor;      /// surface color and emission (light)
+	float _transparency, _reflection;		/// surface transparency and reflectivity
+	Sphere() = default;
 	Sphere(
-		const Vec3f &c,
-		const float &r,
-		const Vec3f &sc,
-		const float &refl = 0,
-		const float &transp = 0,
-		const Vec3f &ec = 0) :
-		center(c), radius(r), radius2(r * r), surfaceColor(sc), emissionColor(ec),
-		transparency(transp), reflection(refl)
-	{ /* empty */
-	}
+		const Vec3f& center,
+		const float& radius,
+		const Vec3f& surfaceColor,
+		const float& reflection = 0,
+		const float& transparency = 0,
+		const Vec3f& emmisionColor = 0) :
+		_center(center), _radius(radius), _radiusSqr(radius* radius), _surfaceColor(surfaceColor), _emissionColor(emmisionColor),
+		_transparency(transparency), _reflection(reflection) { }
+
 	//[comment]
 	// Compute a ray-sphere intersection using the geometric solution
 	//[/comment]
-	bool intersect(const Vec3f &rayorig, const Vec3f &raydir, float &t0, float &t1) const
+	bool intersect(const Vec3f& rayorig, const Vec3f& raydir, float& t0, float& t1) const
 	{
-		Vec3f l = center - rayorig;
+		Vec3f l = _center - rayorig;
 		float tca = l.dot(raydir);
 		if (tca < 0) return false;
 		float d2 = l.dot(l) - tca * tca;
-		if (d2 > radius2) return false;
-		float thc = sqrt(radius2 - d2);
+		if (d2 > _radiusSqr) return false;
+		float thc = sqrt(_radiusSqr - d2);
 		t0 = tca - thc;
 		t1 = tca + thc;
 
@@ -120,7 +85,7 @@ public:
 //[/comment]
 #define MAX_RAY_DEPTH 5
 
-float mix(const float &a, const float &b, const float &mix)
+float mix(const float& a, const float& b, const float& mix)
 {
 	return b * mix + a * (1 - mix);
 }
@@ -136,10 +101,10 @@ float mix(const float &a, const float &b, const float &mix)
 // the background color.
 //[/comment]
 Vec3f trace(
-	const Vec3f &rayorig,
-	const Vec3f &raydir,
-	const std::vector<Sphere> &spheres,
-	const int &depth)
+	const Vec3f& rayorig,
+	const Vec3f& raydir,
+	const std::vector<Sphere>& spheres,
+	const int& depth)
 {
 	//if (raydir.length() != 1) std::cerr << "Error " << raydir << std::endl;
 	float tnear = INFINITY;
@@ -159,7 +124,7 @@ Vec3f trace(
 	if (!sphere) return Vec3f(2);
 	Vec3f surfaceColor = 0; // color of the ray/surfaceof the object intersected by the ray
 	Vec3f phit = rayorig + raydir * tnear; // point of intersection
-	Vec3f nhit = phit - sphere->center; // normal at the intersection point
+	Vec3f nhit = phit - sphere->_center; // normal at the intersection point
 	nhit.normalize(); // normalize normal direction
 					  // If the normal and the view direction are not opposite to each other
 					  // reverse the normal direction. That also means we are inside the sphere so set
@@ -168,7 +133,7 @@ Vec3f trace(
 	float bias = 1e-4; // add some bias to the point from which we will be tracing
 	bool inside = false;
 	if (raydir.dot(nhit) > 0) nhit = -nhit, inside = true;
-	if ((sphere->transparency > 0 || sphere->reflection > 0) && depth < MAX_RAY_DEPTH) {
+	if ((sphere->_transparency > 0 || sphere->_reflection > 0) && depth < MAX_RAY_DEPTH) {
 		float facingratio = -raydir.dot(nhit);
 		// change the mix value to tweak the effect
 		float fresneleffect = mix(pow(1 - facingratio, 3), 1, 0.1);
@@ -179,26 +144,26 @@ Vec3f trace(
 		Vec3f reflection = trace(phit + nhit * bias, refldir, spheres, depth + 1);
 		Vec3f refraction = 0;
 		// if the sphere is also transparent compute refraction ray (transmission)
-		if (sphere->transparency) {
+		if (sphere->_transparency) {
 			float ior = 1.1, eta = (inside) ? ior : 1 / ior; // are we inside or outside the surface?
 			float cosi = -nhit.dot(raydir);
 			float k = 1 - eta * eta * (1 - cosi * cosi);
-			Vec3f refrdir = raydir * eta + nhit * (eta *  cosi - sqrt(k));
+			Vec3f refrdir = raydir * eta + nhit * (eta * cosi - sqrt(k));
 			refrdir.normalize();
 			refraction = trace(phit - nhit * bias, refrdir, spheres, depth + 1);
 		}
 		// the result is a mix of reflection and refraction (if the sphere is transparent)
 		surfaceColor = (
 			reflection * fresneleffect +
-			refraction * (1 - fresneleffect) * sphere->transparency) * sphere->surfaceColor;
+			refraction * (1 - fresneleffect) * sphere->_transparency) * sphere->_surfaceColor;
 	}
 	else {
 		// it's a diffuse object, no need to raytrace any further
 		for (unsigned i = 0; i < spheres.size(); ++i) {
-			if (spheres[i].emissionColor.x > 0) {
+			if (spheres[i]._emissionColor.x > 0) {
 				// this is a light
 				Vec3f transmission = 1;
-				Vec3f lightDirection = spheres[i].center - phit;
+				Vec3f lightDirection = spheres[i]._center - phit;
 				lightDirection.normalize();
 				for (unsigned j = 0; j < spheres.size(); ++j) {
 					if (i != j) {
@@ -209,13 +174,13 @@ Vec3f trace(
 						}
 					}
 				}
-				surfaceColor += sphere->surfaceColor * transmission *
-					std::max(float(0), nhit.dot(lightDirection)) * spheres[i].emissionColor;
+				surfaceColor += sphere->_surfaceColor * transmission *
+					std::max(float(0), nhit.dot(lightDirection)) * spheres[i]._emissionColor;
 			}
 		}
 	}
 
-	return surfaceColor + sphere->emissionColor;
+	return surfaceColor + sphere->_emissionColor;
 }
 
 //[comment]
@@ -223,7 +188,7 @@ Vec3f trace(
 // trace it and return a color. If the ray hits a sphere, we return the color of the
 // sphere at the intersection point, else we return the background color.
 //[/comment]
-void render(const std::vector<Sphere> &spheres, int iteration)
+void render(const std::vector<Sphere>& spheres, int iteration)
 {
 	// quick and dirty
 	unsigned width = 640, height = 480;
@@ -232,10 +197,10 @@ void render(const std::vector<Sphere> &spheres, int iteration)
 
 	// Recommended Production Resolution
 	//unsigned width = 1920, height = 1080;
-	Vec3f *image = new Vec3f[width * height], *pixel = image;
+	Vec3f* image = new Vec3f[width * height], * pixel = image;
 	float invWidth = 1 / float(width), invHeight = 1 / float(height);
 	float fov = 30, aspectratio = width / float(height);
-	float angle = tan(M_PI * 0.5 * fov / 180.);
+	float angle = tan(M_PI * 0.5 * fov / 180.0f);
 	// Trace rays
 	for (unsigned y = 0; y < height; ++y) {
 		for (unsigned x = 0; x < width; ++x, ++pixel) {
@@ -255,12 +220,14 @@ void render(const std::vector<Sphere> &spheres, int iteration)
 	std::ofstream ofs(filename, std::ios::out | std::ios::binary);
 	ofs << "P6\n" << width << " " << height << "\n255\n";
 	for (unsigned i = 0; i < width * height; ++i) {
-		ofs << (unsigned char)(std::min(float(1), image[i].x) * 255) <<
-			(unsigned char)(std::min(float(1), image[i].y) * 255) <<
-			(unsigned char)(std::min(float(1), image[i].z) * 255);
+		ofs << (unsigned char)(std::min(1.0f, image[i].x) * 255) <<
+			(unsigned char)(std::min(1.0f, image[i].y) * 255) <<
+			(unsigned char)(std::min(1.0f, image[i].z) * 255);
 	}
 	ofs.close();
+
 	delete[] image;
+	image = nullptr;
 }
 
 void BasicRender()
@@ -272,7 +239,7 @@ void BasicRender()
 	spheres.push_back(Sphere(Vec3f(0.0, 0, -20), 4, Vec3f(1.00, 0.32, 0.36), 1, 0.5)); // The radius paramter is the value we will change
 	spheres.push_back(Sphere(Vec3f(5.0, -1, -15), 2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
 	spheres.push_back(Sphere(Vec3f(5.0, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), 1, 0.0));
-	
+
 	// This creates a file, titled 1.ppm in the current working directory
 	render(spheres, 1);
 
@@ -326,25 +293,31 @@ void SmoothScaling()
 	std::vector<Sphere> spheres;
 	// Vector structure for Sphere (position, radius, surface color, reflectivity, transparency, emission color)
 
+	spheres.resize(4);
+	spheres[0] = Sphere(Vec3f(0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0);
+	spheres[1] = Sphere(Vec3f(0.0, 0, -20), 0 / 100, Vec3f(1.00, 0.32, 0.36), 1, 0.5); // Radius++ change here
+	spheres[2] = Sphere(Vec3f(5.0, -1, -15), 2, Vec3f(0.90, 0.76, 0.46), 1, 0.0);
+	spheres[3] = Sphere(Vec3f(5.0, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), 1, 0.0);
+
 	for (float r = 0; r <= 100; r++)
 	{
-		spheres.push_back(Sphere(Vec3f(0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0));
-		spheres.push_back(Sphere(Vec3f(0.0, 0, -20), r / 100, Vec3f(1.00, 0.32, 0.36), 1, 0.5)); // Radius++ change here
-		spheres.push_back(Sphere(Vec3f(5.0, -1, -15), 2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
-		spheres.push_back(Sphere(Vec3f(5.0, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), 1, 0.0));
+		float radius = r / 100;
+		spheres[1]._radius = radius;
+		spheres[1]._radiusSqr = radius * radius;
+
 		render(spheres, r);
 		std::cout << "Rendered and saved spheres" << r << ".ppm" << std::endl;
-		// Dont forget to clear the Vector holding the spheres.
-		spheres.clear();
-
 	}
+	spheres.clear();
+
+	
 }
 //[comment]
 // In the main function, we will create the scene which is composed of 5 spheres
 // and 1 light (which is also a sphere). Then, once the scene description is complete
 // we render that scene, by calling the render() function.
 //[/comment]
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
 	// This sample only allows one choice per program execution. Feel free to improve upon this
 	srand(13);
@@ -352,83 +325,10 @@ int main(int argc, char **argv)
 	float timeToComplete = 0.0f;
 
 	Timer timer;
+	SmoothScaling();
+	timeToComplete += timer.Mark();
 
-	std::thread threadA = std::thread([&timeToComplete] {
-		Timer timer;
-		SmoothScaling();
-		timeToComplete += timer.Mark();
-	});
-
-	std::thread threadB = std::thread([&timeToComplete] {
-		Timer timer;
-		SmoothScaling();
-		timeToComplete += timer.Mark();
-		});
-
-	std::thread threadC = std::thread([&timeToComplete] {
-		Timer timer;
-		SmoothScaling();
-		timeToComplete += timer.Mark();
-		});
-
-	std::thread threadD = std::thread([&timeToComplete] {
-		Timer timer;
-		SmoothScaling();
-		timeToComplete += timer.Mark();
-		});
-
-	std::thread threadE = std::thread([&timeToComplete] {
-		Timer timer;
-		SmoothScaling();
-		timeToComplete += timer.Mark();
-		});
-
-	std::thread threadF = std::thread([&timeToComplete] {
-		Timer timer;
-		SmoothScaling();
-		timeToComplete += timer.Mark();
-		});
-
-	std::thread threadG = std::thread([&timeToComplete] {
-		Timer timer;
-		SmoothScaling();
-		timeToComplete += timer.Mark();
-		});
-
-	std::thread threadH = std::thread([&timeToComplete] {
-		Timer timer;
-		SmoothScaling();
-		timeToComplete += timer.Mark();
-		});
-
-	std::thread threadI = std::thread([&timeToComplete] {
-		Timer timer;
-		SmoothScaling();
-		timeToComplete += timer.Mark();
-		});
-
-	std::thread threadJ = std::thread([&timeToComplete] {
-		Timer timer;
-		SmoothScaling();
-		timeToComplete += timer.Mark();
-		});
-
-
-	threadA.join();
-	threadB.join();
-	threadC.join();
-	threadD.join();
-	threadE.join();
-	threadF.join();
-	threadG.join();
-	threadH.join();
-	threadI.join();
-	threadJ.join();
-
-	float averageTime = timeToComplete / 8;
-
-	std::cout << "Average Time: " << averageTime << std::endl;
-
+	std::cout << "Time to complete: " << timeToComplete << std::endl;
 
 	//BasicRender();
 	//SimpleShrinking();
